358225d4926609ce4a97b6a902cdb18670b901cb
Jonathan Thomas Dean <JonathanTomDean@Gmail.com>
2021-02-25 19:31:33 -0500

    Finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import '../css/App.css';
+import NumberOfStudents from './NumberOfStudents.js';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<NumberOfStudents />
+		</div>
+	);
 }
 
 export default App;
--- src/components/NumberOfStudents.js
+++ src/components/NumberOfStudents.js
@@ -3,44 +3,99 @@ import React, { useState, useEffect } from 'react';
 export default function NumberOfStudents(props) {
 	const [waitListedStudents, setWaitListedStudents] = useState(0);
 	const [enrolledStudents, setEnrolledStudents] = useState(0);
+	const [nEnrolledStudents, setNEnrolledStudents] = useState(0);
+	const [nWaitListedStudents, setNWaitListedStudents] = useState(0);
 
-	useEffect(() => {});
+	function submitCheck(e) {
+		// console.log(e.target.value);
+		e.preventDefault();
+	}
+
+	function increaseNStudents(
+		e,
+		getStudentType,
+		setStudentType,
+		getNStudents,
+		setNStudents
+	) {
+		setStudentType(getStudentType + getNStudents);
+		setNStudents(0);
+	}
+
+	function handleNStudents(e, getNStudents, setNStudents) {
+		setNStudents(parseInt(e.target.value));
+		console.log(`N STUDENTS: ${getNStudents}`);
+	}
 
 	return (
 		<div>
 			{/* Enrolled Students */}
 			<div>
-				<h1>Enrolled Students</h1>
+				<h1>Enrolled Students: {enrolledStudents}</h1>
 				<button onClick={() => setEnrolledStudents(enrolledStudents + 1)}>
 					Add 1 Enrolled Student
 				</button>
 				<h3>Add Multiple Enrolled Students</h3>
-				<input
-                    name="enrolled"
-                    type="button" 
-                    onChange={(e) => console.log(e)} 
-                />
-				<label htmlFor="enrolled">Increase Students</label>
+				<form onSubmit={(e) => submitCheck(e)}>
+					<label htmlFor="enrolled">Increase Students:</label>
+					<br />
+					<input
+						name="enrolled"
+						defaultValue={0}
+						value={nEnrolledStudents}
+						onChange={(e) =>
+							handleNStudents(e, nEnrolledStudents, setNEnrolledStudents)
+						}
+					/>
+					<input
+						type="submit"
+						value="Submit"
+						onClick={(e) =>
+							increaseNStudents(
+								e,
+								enrolledStudents,
+								setEnrolledStudents,
+								nEnrolledStudents,
+								setNEnrolledStudents
+							)
+						}
+					/>
+				</form>
 			</div>
 
 			{/* Waitlisted Students */}
 			<div>
-				<h1>Waitlisted Students</h1>
+				<h1>Waitlisted Students: {waitListedStudents}</h1>
 				<button onClick={() => setWaitListedStudents(waitListedStudents + 1)}>
 					Add 1 Waitlisted Student
 				</button>
 				<h3>Add Multiple Waitlisted Students</h3>
-				<input
-					name="waitlisted"
-					type="button"
-					onChange={(e) => console.log(e)}
-				/>
-				<label htmlFor="waitlisted">Increase Students</label>
+				<form onSubmit={submitCheck}>
+					<label htmlFor="waitlisted">Increase Students </label>
+					<br />
+					<input
+						name="waitlisted"
+						defaultValue={0}
+						value={nWaitListedStudents}
+						onChange={(e) =>
+							handleNStudents(e, nWaitListedStudents, setNWaitListedStudents)
+						}
+					/>
+					<input
+						type="submit"
+						value="Submit"
+						onClick={(e) =>
+							increaseNStudents(
+								e,
+								waitListedStudents,
+								setWaitListedStudents,
+								nWaitListedStudents,
+								setNWaitListedStudents
+							)
+						}
+					/>
+				</form>
 			</div>
 		</div>
 	);
-}
-
-function students(studentsAmount) {
-	return studentsAmount + 1;
 }
